a213652f7745dd18ce51c62962357d064913ec67
madhancvf <maddycvf29@gmail.com>
2022-03-18 11:59:51 +0530

    sample123

---
+++ src/test/java/sample.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class sample {
+@Test
+private void Sample() {
+	System.out.println("TEst 111");
+	System.out.println("TEst 222");
+	System.out.println("TEst 333");
+
+}
+
+}
